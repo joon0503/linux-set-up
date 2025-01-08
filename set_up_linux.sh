dd2a36b65060861681f69a2fd86265befd9a80b4
Seungjoon Lee <sjoon0503@gmail.com>
2025-01-08 10:15:09 +0900

    update

--- set_up_linux.sh
+++ set_up_linux.sh
@@ -1,5 +1,25 @@
 #!/bin/sh
-sudo apt-get install -y curl
+sudo apt-get install -y curl git vim zsh cmake build-essential
 
 # install omz
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+####
+# install powerline-10k
+####
+
+# 1. install recommended font
+curl -L -o /usr/local/share/fonts/MesloLGS%20NF%20Regular.ttf ~/Downloads/test.file https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
+curl -L -o /usr/local/share/fonts/MesloLGS%20NF%20Bold.ttf ~/Downloads/test.file https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
+curl -L -o /usr/local/share/fonts/MesloLGS%20NF%20Italic.ttf ~/Downloads/test.file https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
+curl -L -o /usr/local/share/fonts/MesloLGS%20NF%20Bold%20Italic.ttf ~/Downloads/test.file https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
+
+fc-cache -f
+
+# 2. intsall powerline-10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# 3. replace zsh theme
+sed -i 's|^ZSH_THEME="robbyrussell"|ZSH_THEME="powerlevel10k/powerlevel10k"|' ~/.zshrc
+
+# 4. blah
